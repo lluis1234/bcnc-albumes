4a3dbab97156c5e042733cbe0e03e5c7daea01cb
lluis <lluis.oliveras.boada@gmail.com>
2023-11-03 13:23:02 +0100

    Initial commit

---
+++ src/main/java/com/lluis/bcncalbumes/BcncAlbumesApplication.java
@@ -0,0 +1,13 @@
+package com.lluis.bcncalbumes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcncAlbumesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcncAlbumesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lluis/bcncalbumes/BcncAlbumesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lluis.bcncalbumes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcncAlbumesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
